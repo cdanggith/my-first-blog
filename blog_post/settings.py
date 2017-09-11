3077a9afc3b0c2813bdf63344c05fb21503d1cf7
chi_dang <cscdang@gmail.com>
2017-09-11 10:22:44 -0700

    My Django Girls app, first commit

--- blog_post/settings.py
+++ blog_post/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'lmv*_rwwqbh*qs(y-@)idp$u)6n8hrow!uuujgh#55n0@d2*j*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'cdangpyth.pythonanywhere.com']
 
 
 # Application definition
